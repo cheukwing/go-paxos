5031e46777e34480d22dd9ab9a2b3d992e2e2aa1
Michael Tsang <michaeltsang1998@gmail.com>
2019-04-02 23:40:26 +0100

    begin proposer

--- proposer.go
+++ proposer.go
@@ -4,6 +4,7 @@ type proposer struct {
 	id        int
 	pv        int
 	pn        int
+	receives  chan message
 	acceptors []chan Message
 }
 
@@ -15,7 +16,24 @@ func NewProposer(id int, acceptors []chan message) *proposer {
 	p.acceptors = acceptors
 }
 
+func (p *proposer) run() {
+	p.prepare()
+	responded := make(map[int]int)
+}
+
 func (p *proposer) prepare() {
 	p.pn++
+	msg := NewPrepareMessage(p.id, p.pn)
+	p.broadcast(msg)
+}
 
+func (p *proposer) accept() {
+	msg := NewAcceptMessage(p.id, p.pn, p.pv)
+	p.broadcast(msg)
+}
+
+func (p *proposer) broadcast(msg message) {
+	for _, acceptor := range p.acceptors {
+		acceptor <- message
+	}
 }
