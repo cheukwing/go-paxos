c7611b83c1be894efc4c630caa9da672b5d2e546
Michael Tsang <michaeltsang1998@gmail.com>
2019-04-06 16:45:37 +0100

    Add tests

--- paxos_test.go
+++ paxos_test.go
@@ -20,10 +20,90 @@ func TestBroadcast(t *testing.T) {
 }
 
 func TestSingleProposer(t *testing.T) {
-	n := NewNetwork(1, 1, 1, []int{1})
+	n := NewNetwork(1, 1, 2, []int{1})
 	go n.acceptors[0].run()
 	go n.proposers[0].run()
-	if n.learners[0].run() != 1 {
+	if n.learners[0].run() != n.learners[1].run() {
+		t.Errorf("Did not receive the proposed value!")
+	}
+}
+
+func TestTwoProposersSameValue(t *testing.T) {
+	n := NewNetwork(2, 1, 2, []int{2, 2})
+	go n.acceptors[0].run()
+	go n.proposers[0].run()
+	go n.proposers[1].run()
+	if n.learners[0].run() != n.learners[1].run() {
+		t.Errorf("Did not receive the proposed value!")
+	}
+}
+
+func TestTwoProposersDifferentValue(t *testing.T) {
+	n := NewNetwork(2, 1, 2, []int{1, 2})
+	go n.acceptors[0].run()
+	go n.proposers[0].run()
+	go n.proposers[1].run()
+	if n.learners[0].run() != n.learners[1].run() {
+		t.Errorf("Did not receive the proposed value!")
+	}
+}
+
+func TestManyProposersDifferentValues(t *testing.T) {
+	n := NewNetwork(5, 1, 2, []int{1, 2, 3, 4, 5})
+	go n.acceptors[0].run()
+	for _, p := range n.proposers {
+		go p.run()
+	}
+	if n.learners[0].run() != n.learners[1].run() {
+		t.Errorf("Did not receive the proposed value!")
+	}
+}
+
+func TestTwoAcceptors(t *testing.T) {
+	n := NewNetwork(1, 2, 2, []int{3})
+	go n.acceptors[0].run()
+	go n.acceptors[1].run()
+	go n.proposers[0].run()
+	if n.learners[0].run() != n.learners[1].run() {
+		t.Errorf("Did not receive the proposed value!")
+	}
+}
+
+func TestManyProposersManyAcceptorsSameValue(t *testing.T) {
+	n := NewNetwork(5, 5, 2, []int{1, 1, 1, 1, 1})
+	for _, a := range n.acceptors {
+		go a.run()
+	}
+	for _, p := range n.proposers {
+		go p.run()
+	}
+	if n.learners[0].run() != n.learners[1].run() {
+		t.Errorf("Did not receive the proposed value!")
+	}
+}
+
+func TestManyProposersManyAcceptorsDifferentValues(t *testing.T) {
+	n := NewNetwork(5, 5, 2, []int{1, 2, 3, 4, 5})
+	for _, a := range n.acceptors {
+		go a.run()
+	}
+	for _, p := range n.proposers {
+		go p.run()
+	}
+	if n.learners[0].run() != n.learners[1].run() {
+		t.Errorf("Did not receive the proposed value!")
+	}
+}
+
+func TestManyProposersManyAcceptorsSemiSameValues(t *testing.T) {
+	n := NewNetwork(5, 5, 2, []int{1, 1, 1, 2, 2})
+	for _, a := range n.acceptors {
+		go a.run()
+	}
+	for _, p := range n.proposers {
+		go p.run()
+	}
+	if n.learners[0].run() != n.learners[1].run() {
 		t.Errorf("Did not receive the proposed value!")
 	}
 }
